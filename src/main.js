acd6c8d82d2ddaac56bbd1fbd531278f4de0abbf
roaayala <roaayala.ahm@gmail.com>
2026-06-24 12:56:44 +0800

    setup projet structure

--- src/main.js
+++ src/main.js
@@ -2,34 +2,39 @@ import "./style.css";
 import van from "vanjs-core";
 
 const fecthData = async () => {
-    const url = "https://api.myquran.com/v3/sholat/kabkota/find/samarinda";
+  const url = "https://api.myquran.com/v3/sholat/kabkota/find/samarinda";
 
-    try {
-        const response = await fetch(url);
-        if (!response.ok) {
-            throw new Error(response.status);
-        }
-        const result = await response.json();
-        return result;
-    } catch (error) {
-        return error;
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(response.status);
     }
+    const result = await response.json();
+    return result;
+  } catch (error) {
+    return error;
+  }
 };
 
 const { h1 } = van.tags;
 
 const PageHeader = () => {
-    const title = van.state("Nama Kota ...");
+  const title = van.state("Nama Kota ...");
 
-    fecthData().then((res) => {
-        if (res.status && res.data.length > 0) {
-            title.val = res.data[0].lokasi;
-        } else {
-            title.val = "Gagal narik data nih bro";
-        }
-    });
+  fecthData().then((res) => {
+    if (res.status && res.data.length > 0) {
+      title.val = res.data[0].lokasi;
+    } else {
+      title.val = "Gagal narik data nih bro";
+    }
+  });
 
-    return h1({ class: "font-bold" }, () => title.val);
+  return h1(
+    {
+      class: "font-bold",
+    },
+    () => title.val,
+  );
 };
 
-van.add(document.getElementById("app"), PageHeader);
+van.add(document.getElementById("app"), PageHeader());
