5d1e21a76965e9acb565c352e29c2ed22cb36f5d
roaayala <roaayala.ahm@gmail.com>
2026-06-25 12:56:35 +0800

    successfully pulling prayer schedule data

--- src/main.js
+++ src/main.js
@@ -7,6 +7,7 @@ const { h1 } = van.tags;
 const App = () => {
   const userLocation = van.state({});
   const cityId = van.state("");
+  const schedule = van.state([]);
 
   const getVisitorInfo = async () => {
     try {
@@ -15,6 +16,12 @@ const App = () => {
 
       const id = await getCityId(user.city);
       cityId.val = id;
+
+      fetch(
+        `https://api.myquran.com/v3/sholat/jadwal/${cityId.val}/today?tz=Asia%2FJakarta`,
+      )
+        .then((data) => data.json())
+        .then((data) => console.log(data.data.jadwal));
     } catch (error) {
       console.log(error);
       cityId.val = "Kabupaten/Kota ga ketemu nih";
