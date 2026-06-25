3e190580ab8183b86d1e72149705b4b00b7ae687
roaayala <roaayala.ahm@gmail.com>
2026-06-25 12:13:20 +0800

    user approximately location

--- src/main.js
+++ src/main.js
@@ -1,40 +1,26 @@
 import "./style.css";
 import van from "vanjs-core";
-
-const fecthData = async () => {
-  const url = "https://api.myquran.com/v3/sholat/kabkota/find/samarinda";
-
-  try {
-    const response = await fetch(url);
-    if (!response.ok) {
-      throw new Error(response.status);
-    }
-    const result = await response.json();
-    return result;
-  } catch (error) {
-    return error;
-  }
-};
+import { getUserApproximatelyLocation } from "./utils/api";
 
 const { h1 } = van.tags;
 
-const PageHeader = () => {
-  const title = van.state("Nama Kota ...");
+const App = () => {
+  const userLocation = van.state({});
 
-  fecthData().then((res) => {
-    if (res.status && res.data.length > 0) {
-      title.val = res.data[0].lokasi;
-    } else {
-      title.val = "Gagal narik data nih bro";
-    }
-  });
+  getUserApproximatelyLocation()
+    .then((user) => {
+      userLocation.val = user;
+    })
+    .catch((error) => {
+      userLocation.val = error;
+    });
 
   return h1(
     {
       class: "font-bold",
     },
-    () => title.val,
+    "Hello",
   );
 };
 
-van.add(document.getElementById("app"), PageHeader());
+van.add(document.getElementById("app"), App());
---
+++ src/models/User.js
@@ -0,0 +1,3 @@
+export default function createUser({ city, province, country }) {
+  return { city, province, country };
+}
